7434788c2d465e5661154e005a23ba2bd4558f14
trevorsaint <hello@trevorsaint.uk>
2021-04-10 10:51:56 +0100

    Removed role attributes. Updated 404 page due to incorrect closing tag

--- gulpfile.js
+++ gulpfile.js
@@ -94,7 +94,7 @@ gulp.task('nunjucks', function() {
       ],
       data: {
         serviceName: 'Trevor Saint',
-        rootPath: 'https://trevorsaint.uk/'
+        rootPath: '/'
       }
     }))
     .pipe(gulp.dest(configPaths.public))
