4b637a607e2df9ed8ddff33919b7e61e6b3806dc
trevorsaint <hello@trevorsaint.uk>
2021-08-21 18:21:38 +0100

    Fixed invalid link

--- gulpfile.js
+++ gulpfile.js
@@ -88,7 +88,7 @@ gulp.task('nunjucks', function() {
       ],
       data: {
         serviceName: 'Trevor Saint',
-        rootPath: '/'
+        rootPath: 'https://trevorsaint.uk/'
       }
     }))
     .pipe(gulp.dest(configPaths.public))
